f99241a5c18fbfeefd2fb068db3d0ece470b6937
was1809-design <was1809@gmail.com>
2026-09-02 01:48:42 -0700

    Corrige inicialização e cache de sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="jornada-v3";
-const FILES=["./","index.html","style.css","app.js","manifest.json","icon.svg"];
-self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
-self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
-self.addEventListener("fetch",event=>event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request))));
+const CACHE="jornada-v4";
+const FILES=["./","index.html","style.css?v=4","app.js?v=4","manifest.json","icon.svg"];
+self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)))});
+self.addEventListener("activate",event=>{event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))]))});
+self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;const url=new URL(event.request.url);if(url.origin===location.origin){event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match("./"))))}});
