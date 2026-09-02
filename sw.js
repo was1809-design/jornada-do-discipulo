b2e0594ecedc98cb350cff67383d043337c760dc
was1809-design <was1809@gmail.com>
2026-09-02 05:29:34 -0300

    Adiciona sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="jornada-v2";
+const FILES=["./","index.html","style.css","app.js","manifest.json","icon.svg"];
+self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
+self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener("fetch",event=>event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request))));
