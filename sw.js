a1b62693e44d21b012bea7c4709e174d3cfe53ef
was1809-design <was1809@gmail.com>
2026-09-02 05:44:26 -0300

    Atualiza sw.js para a Vila da Bondade jogável

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="jornada-v2";
+const CACHE="jornada-v3";
 const FILES=["./","index.html","style.css","app.js","manifest.json","icon.svg"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
