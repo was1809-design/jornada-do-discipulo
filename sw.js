0e4d64738836886923b0123d0ccce718d6a8e22f
was1809-design <was1809@gmail.com>
2026-09-02 17:57:58 -0700

    Refresh fullscreen game assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="jornada-v6";
-const FILES=["./","index.html","style.css?v=5","hotfix.css?v=5","app.js?v=5","manifest.json","icon.svg"];
+const CACHE="jornada-v7";
+const FILES=["./","index.html","style.css?v=7","hotfix.css?v=7","fullscreen.css?v=7","app.js?v=7","manifest.json","icon.svg"];
 self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)))});
 self.addEventListener("activate",event=>{event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))]))});
 self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;const url=new URL(event.request.url);if(url.origin===location.origin){event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match("./"))))}});
